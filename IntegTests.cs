759c5c00e06193a2aef5a29bea634175e3592921
Peter Varga <pvarga@rpmmoves.com>
2024-01-16 16:26:27 -0500

    .

--- IntegTests.cs
+++ IntegTests.cs
@@ -15,18 +15,34 @@ namespace IntegrationTests
         }
 
         [Fact, TestPriority(0)]
-        public async Task Test1()
+        public async Task GetRead()
         {
             HttpStatusCode expectedStatusCode = HttpStatusCode.OK;
             // Arrange
             var client = await _httpClientFixture.GetAuthorizedHttpClientAsync();
-            string getUri = $"{_httpClientFixture.BaseUrl}/api";
+            string getUri = $"{_httpClientFixture.BaseUrl}/api/turvoOtherDocument/download/4c452066-6c66-4edc-9a8c-e1fa2078cac7";
 
             // Act
             var response = await client.GetAsync(getUri);
 
             // Assert
             Assert.Equal(expectedStatusCode, response.StatusCode);
+        }
+        [Fact, TestPriority(1)]
+        public async Task PostUpdate()
+        {
+            // Arrange
+            var client = await _httpClientFixture.GetAuthorizedHttpClientAsync();
+            string postUri = $"{_httpClientFixture.BaseUrl}/api/turvoOtherDocument/withAttachments";
+
+            var postRequestContent = TestHelper.GetHttpContentFromSampleData("SOMEDOC.pdf");
+            postRequestContent.Headers.Add("Authorization", "jF9gOMjU7ZgH0WL1xqbHC4SOFVDj8FuXlSdwBUoAAcg=");
+
+            // Act
+            var putResponse = await client.PostAsync(postUri, postRequestContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, putResponse.StatusCode);
         }
     }
 }
